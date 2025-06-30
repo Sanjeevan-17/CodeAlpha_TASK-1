307324fe2cd9d5c8d49e77e91010a93e1c13169f
Sanjeevan S <shundi.1701@gmail.com>
2025-06-30 16:46:18 +0530

    Update firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -2,9 +2,9 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # Load service account key
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json") #ADD FIREBASE JSON FILE
 
 # Initialize Firebase app
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://task-1-ba4e7-default-rtdb.firebaseio.com/'
+    'databaseURL': 'https:/-------' #ADD YOU ARE FIREBASE API
 })
